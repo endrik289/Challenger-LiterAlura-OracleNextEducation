3e93cb6a43f809e109908acea527c0b5f39498a3
endrik289 <endrik289@gmail.com>
2026-02-27 13:05:59 -0300

    Fiz Algumas Alteraçoes no Pom.xml tambem criei o convertando dados e o analizando a resposta json

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,11 +1,13 @@
 package br.com.alura.literalura;
 
+import br.com.alura.literalura.model.DadosResultados;
 import br.com.alura.literalura.service.ConsumoApi;
+import br.com.alura.literalura.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+@SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -16,6 +18,10 @@ public class LiteraluraApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         var consumoApi = new ConsumoApi();
         var json = consumoApi.obterDados("https://gutendex.com/books/");
-        System.out.println(json);
+
+        var conversor = new ConverteDados();
+        var dados = conversor.obterDados(json, DadosResultados.class);
+
+        System.out.println(dados);
     }
 }
